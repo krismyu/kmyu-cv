6ad7bb1bfcc731ce658d0a14653e481bd3764868
Kristine M. Yu <krisyu@linguist.umass.edu>
2014-12-08 15:42:34 -0800

    Updated LDC paper pub

---
+++ auto/kmyu-cv.el
@@ -0,0 +1,52 @@
+(TeX-add-style-hook "kmyu-cv"
+ (lambda ()
+    (LaTeX-add-labels
+     "#1")
+    (TeX-add-symbols
+     "endbibsection"
+     "endbibenum"
+     '("makeheading" ["argument"] 1)
+     '("email" 1)
+     '("emaillink" 1)
+     '("fixinnerlist" 1)
+     '("fixouterlist" 1)
+     '("fixendlist" 1)
+     '("footref" 1)
+     '("footlabel" 2)
+     "blankline"
+     "halfblankline"
+     "BibTeX"
+     "Matlab"
+     "oldendbibenum"
+     "oldendbibsection"
+     "originalItem"
+     "oldItem"
+     "item")
+    (TeX-run-style-hooks
+     "url"
+     "doi"
+     "hyperref"
+     "color"
+     "lastpage"
+     "fancyhdr"
+     "gitinfo"
+     "enumitem"
+     "shortlabels"
+     "geometry"
+     "includemp"
+     "margin=1in"
+     "marginparsep=.05in"
+     "marginparwidth=1.2in"
+     "paper=letterpaper"
+     "extdash"
+     "shortcuts"
+     "calc"
+     "times"
+     ""
+     "fontenc"
+     "T1"
+     "latex2e"
+     "art10"
+     "article"
+     "10pt")))
+
